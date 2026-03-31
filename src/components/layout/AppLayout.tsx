60753810005dbee7ba6349f4856813a9e1f224e6
Georgi <49676810+georgiyanakiev@users.noreply.github.com>
2026-04-01 01:23:01 +0300

    Updated AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -5,10 +5,8 @@ export default function AppLayout() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <TopBar variant="hotel" />
-      <main className="flex-1">
-        <div className="px-4 py-4 lg:px-6 lg:py-6 max-w-[1440px] mx-auto">
-          <Outlet />
-        </div>
+      <main className="flex-1 px-4 py-4 lg:px-6 lg:py-6">
+        <Outlet />
       </main>
     </div>
   );
